569af9caacf8f3a8b84c484009ba2cb050ac2190
Marie Denney <mldenne@gmail.com>
2016-08-08 19:37:10 -0400

    Added billing addresses resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808233457) do
+ActiveRecord::Schema.define(version: 20160808233634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "billing_addresses", force: :cascade do |t|
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zipcode"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
