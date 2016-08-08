a320015c6b3ae288e9eee077429f0c2d136f80c0
Marie Denney <mldenne@gmail.com>
2016-08-08 19:32:47 -0400

    Added order resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808233040) do
+ActiveRecord::Schema.define(version: 20160808233206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "order_id"
+    t.integer  "order_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
