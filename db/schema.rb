c331ac0d1b294d959c93feb8aae7d2057b55a0b7
Marie Denney <mldenne@gmail.com>
2016-08-08 19:31:15 -0400

    Added product resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808225204) do
+ActiveRecord::Schema.define(version: 20160808233040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "sku"
+    t.integer  "price"
+    t.integer  "inventory"
+    t.integer  "year"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
