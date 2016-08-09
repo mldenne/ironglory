025502f0aa40afd07e858f8df54ba3a1712eef99
Marie Denney <mldenne@gmail.com>
2016-08-09 16:36:11 -0400

    Updated order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,11 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :order_number
+
+  belongs_to :can_order, polymorphic: true
+  belongs_to :shipping_address
+  belongs_to :billing_address
+
+  has_many :products, through: :orderings
+  has_many :orderings
+  
 end
