14bbe74fb2bd7f4f0a67eb21d2a67521e52d1986
Marie Denney <mldenne@gmail.com>
2016-08-09 12:28:03 -0400

    Added relationships to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+
+  belongs_to :can_order, polymorphic: true
+  belongs_to :shipping_address
+  belongs_to :billing_address
+
+  has_many :products, through: :orderings
+  has_many :orderings
+
 end
