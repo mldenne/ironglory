b38d93c567badc1082d98cc95b6f8acfc4fe3e58
Marie Denney <mldenne@gmail.com>
2016-08-09 16:50:40 -0400

    Updated billing address serializer

--- app/serializers/billing_address_serializer.rb
+++ app/serializers/billing_address_serializer.rb
@@ -1,3 +1,8 @@
 class BillingAddressSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :street_address, :city, :state, :zipcode
+
+  belongs_to :can_be_billed, polymorphic: true
+
+  has_many :orders
+
 end
