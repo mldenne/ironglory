a472b2ef50ca2016f9ddd584197cafd063348b22
Marie Denney <mldenne@gmail.com>
2016-08-09 16:28:01 -0400

    Updated product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,9 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :sku, :price, :inventory, :year, :description
+
+  belongs_to :category
+
+  has_many :orders, through: :orderings
+  has_many :reviews
+
 end
