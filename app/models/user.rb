6495078d69c46971b035e6d4a0dd3c0af8742165
Marie Denney <mldenne@gmail.com>
2016-08-08 20:55:03 -0400

    Updated relationships in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  has_many :shipping_address
-  has_many :billing_address
+  has_many :orders
+  has_many :shipping_addresses
+  has_many :billing_addresses
   has_many :reviews
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :firstname, presence: true
   validates :lastname, presence: true
-  validates :shipping_address, presence: true
-  validates :billing_address, presence: true
 
 end
