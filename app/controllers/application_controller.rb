8f1dde58b303b81abd436661002a43ed2de97fa2
Marie Denney <mldenne@gmail.com>
2016-08-10 16:44:24 -0400

    Updated with current order info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::Base
   helper_method :current_order
 
   def current_order
-    if !session[:identifier].nil?
-      Order.find(session[:identifier])
+    if session[:user_id]
+      @user = User.find_by(username: session[:user_id])
+      @order = @user.orders.where.not(order_status: 'complete')
+    elsif session[:guest_id]
+      @guest = Guest.find_by(guest_num: session[:user_id])
+      @order = @guest.orders.where.not(order_status: "complete")
     else
-      Order.new
+      @order = Order.new
     end
+    @order
   end
 
 end
