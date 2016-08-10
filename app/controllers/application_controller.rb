e9056e79849b484962b60cc56a1326ca442e63e2
Marie Denney <mldenne@gmail.com>
2016-08-10 15:42:12 -0400

    Added helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_order
+
+  private
+
+  def current_order
+     
+  end
+
 end
