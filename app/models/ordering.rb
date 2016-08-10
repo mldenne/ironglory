2b7b27855091536b068ab43f7b01aa95414dc21d
Marie Denney <mldenne@gmail.com>
2016-08-10 16:03:05 -0400

    Added validations and methods to ordering

--- app/models/ordering.rb
+++ app/models/ordering.rb
@@ -2,7 +2,42 @@ class Ordering < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  validates :quantity, presence: true
+  validate :product_present
+  validate :order_present
+
+  before_save :finalize
+
+  def unit_price
+    if persisted?
+      self.[:unit_price]
+    else
+      product.price
+    end
+  end
+
   def total_ordering_price
     product.price * quantity
+  end
+
+  private
+
+  def product_present
+    if product.nil?
+      errors.add(:product, "Inventory not available for this patch")
+    end
+  end
+
+  def order_present
+    if order.nil?
+      errors.add(:order, "Your cart is empty, please select item to order")
+    end
+  end
+
+  def finalize
+    self[:unit_price] = unit_price
+    self[:total_price] = quantity * self[:unit_price]
+  end
+
   end
 end
