81aa5cf4132bf1c4dc5330b995da9f00340e0493
Marie Denney <mldenne@gmail.com>
2016-08-09 22:09:54 -0400

    Added route for charges

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :products
   resources :users
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
