8d7fd7bac72315d97d9d65805e2bf98044606ed4
Marie Denney <mldenne@gmail.com>
2016-08-10 18:22:54 -0400

    Added create, update, destroy

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,34 @@
 class OrderItemsController < ApplicationController
   def create
+    if session[:guest_id] || session[:user_id]
+      @order = current_order
+      @order_item = @order.order_items.new(order_item_params)
+      @order.save
+    end
   end
 
   def update
+    if session[:guest_id] || session[:user_id]
+      @order = current_order
+      @order_item = @order.order_items.find(params[:id])
+      @order.update_attributes(order_item_params)
+      @order_items = @order.order_items
+    end
   end
 
   def destroy
+    if session[:guest_id] || session[:user_id]
+      @order = current_order
+      @order_item = @order.order_items.find(params [:id])
+      @order_item.destroy
+      @order_items = @order.order_items
+    end
   end
+
+  private
+
+  def order_items_params
+    params.require(:order_item).permit(:quantity, :product_id)
+  end
+
 end
