9ef4d4e958b5872b073c43bcd45e94eb61b19949
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-10 00:37:27 -0400

    guest and user login functionality. Pictures put into refile

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,9 +1,20 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :sku, :price, :inventory, :year, :description
-
   belongs_to :category
-
   has_many :orders, through: :orderings
   has_many :reviews
+  has_one :product_image
+  # has_one :other_sizes
+
+  def product_image
+    Refile.attachment_url(object, :product_image)
+  end
+  # def other_sizes
+  # image_array = []
+  # image_array << {five_hundred: Refile.attachment_url(object, :image, :fit, 500, 500)}
+  # image_array << {one_thousand: Refile.attachment_url(object, :image, :fit, 1000, 1000)}
+  # image_array << {fifty_profile: Refile.attachment_url(object, :image, :limit, 50, 50)}
+  # image_array
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   resources :shipping_addresses
   resources :billing_addresses
   resources :reviews
-  resources :categories
+  resources :categories do
+    resources :products
+  end
   resources :orders
-  resources :products
   resources :users
+
+  root 'categories#index'
+  post '/login' => 'user_sessions#create'
+  get '/check' => 'user_sessions#show'
+  delete '/logout' => 'user_sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
