7d7e916cd7000092d74daf4475ba0dfdafa0ce1b
Marie Denney <mldenne@gmail.com>
2016-08-10 15:47:53 -0400

    Added relationships private methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,11 @@ class Order < ApplicationRecord
   belongs_to :can_order, polymorphic: true
   belongs_to :shipping_address
   belongs_to :billing_address
+  belongs_to :order_status
+
+  before_create :set_order_status
+
+  before_save :update_total_order_price
 
   has_many :products, through: :orderings
   has_many :orderings
@@ -14,5 +19,16 @@ class Order < ApplicationRecord
       end
     order_price
   end
+
+  private
+
+  def set_order_status
+    self.order_status_id = 1
+  end
+
+  def update_total_order_price
+    self[:total_order_price]
+  end
+
 
 end
