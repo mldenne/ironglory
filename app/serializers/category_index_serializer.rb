964eaf6e986fc1c380af8813e035756db1639de2
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-11 13:06:17 -0400

    heroku pics work?

--- app/serializers/category_index_serializer.rb
+++ app/serializers/category_index_serializer.rb
@@ -1,7 +1,8 @@
 class CategoryIndexSerializer < ActiveModel::Serializer
-  attributes :id, :name, :featured_pic
+  attributes :id, :name
+  #need featured_pic ^^
 
-  def featured_pic
-    Refile.attachment_url(object.products.where(featured: true).first, :product_image, :fit, 400, 400)
-  end
+  # def featured_pic
+  #   Refile.attachment_url(object.products.where(featured: true).first, :product_image, :fit, 400, 400)
+  # end
 end
