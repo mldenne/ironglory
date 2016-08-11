1436b0a5907c8cd02b4f9908b1ab716f079c7efb
Marie Denney <mldenne@gmail.com>
2016-08-11 08:33:25 -0400

    Cleaned up order_items_controller and added route

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,28 +1,23 @@
 class OrderItemsController < ApplicationController
   def create
-    if session[:guest_id] || session[:user_id]
-      @order = current_order
-      @order_item = @order.order_items.new(order_item_params)
-      @order.save
-    end
+    @order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    @order.save
+    # session[] = @order_id
   end
 
   def update
-    if session[:guest_id] || session[:user_id]
-      @order = current_order
-      @order_item = @order.order_items.find(params[:id])
-      @order.update_attributes(order_item_params)
-      @order_items = @order.order_items
-    end
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order.update_attributes(order_item_params)
+    @order_items = @order.order_items
   end
 
   def destroy
-    if session[:guest_id] || session[:user_id]
-      @order = current_order
-      @order_item = @order.order_items.find(params [:id])
-      @order_item.destroy
-      @order_items = @order.order_items
-    end
+    @order = current_order
+    @order_item = @order.order_items.find(params [:id])
+    @order_item.destroy
+    @order_items = @order.order_items
   end
 
   private
