e56f4c27bf80cfea76ae763d0ce4a7e1da6eb696
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-11 19:26:42 -0400

    new stuff for shopping cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,14 @@
 class OrdersController < ApplicationController
-  before_action :current_user
-  before_action :current_order
+  before_action :current_user, except: [:fake_order]
+  before_action :current_order, except: [:fake_order]
 
-  def show
-    render json: current_order
+  def update
+    if params[:user_info]
+    end
+  end
+
+  def fake_order
+    @order = Order.find(9)
+    render json: @order, include: ['orderings', 'orderings.product']
   end
 end
