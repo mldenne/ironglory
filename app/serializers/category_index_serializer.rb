41fef45a7294125f341c5259f205dd29482f96d2
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-11 14:43:42 -0400

    featured pic

--- app/serializers/category_index_serializer.rb
+++ app/serializers/category_index_serializer.rb
@@ -1,8 +1,7 @@
 class CategoryIndexSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  #need featured_pic ^^
+  attributes :id, :name, :featured_pic
 
-  # def featured_pic
-  #   Refile.attachment_url(object.products.where(featured: true).first, :product_image, :fit, 400, 400)
-  # end
+  def featured_pic
+    Refile.attachment_url(object.products.where(featured: true).first, :product_image, :fit, 400, 400)
+  end
 end
